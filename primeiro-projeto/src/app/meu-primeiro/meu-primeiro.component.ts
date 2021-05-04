0b140f62470bf8ef972d7d0158a5780f5dd99dba
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-05-04 18:43:43 -0300

    Hello World Angular

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p>Meu primeiro component com Angular 2!</p>
+    `
+})
+class MeuPrimeiroComponent {}
