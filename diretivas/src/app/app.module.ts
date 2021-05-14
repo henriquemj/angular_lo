572115f55ab0f8c566f2e098b568db961b7bffc5
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-05-13 22:56:24 -0300

    diretiva-ngfor component

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule
