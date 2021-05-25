f4469c3fe56c999dd2455b983255a6085b9eb5a3
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-05-25 19:06:02 -0300

    provider locale / criando uma settings service

--- pipes06/src/app/app.module.ts
+++ pipes06/src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {registerLocaleData} from '@angular/common';
+import br from '@angular/common/locales/pt';
+registerLocaleData(br, 'pt-BR');
+
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemplosPipesComponent
+    ExemplosPipesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
