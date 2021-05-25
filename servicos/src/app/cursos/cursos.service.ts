5f775b37a05e90b1e032d5f22c28a3318e5ed7af
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-05-24 22:33:31 -0300

    escopo de instâncias de  serviços

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class CursosService {
 
+    private cursos: string[] = ['Angular 11',' C#','Java'];
+
+    constructor(){
+        console.log('CursosService');
+    }
+
     getCursos() {
-        return ['Angular 11', 'C#','Java'];
+        return this.cursos;
+    }
+
+    addCurso (curso: string) {
+        this.cursos.push(curso);
     }
 }
