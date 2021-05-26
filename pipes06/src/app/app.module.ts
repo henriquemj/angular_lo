332db8fe3a7eb067f19405dd5a0fc00bf0fd5c50
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-05-26 17:33:58 -0300

    pipe puro e impuro

--- pipes06/src/app/app.module.ts
+++ pipes06/src/app/app.module.ts
@@ -10,14 +10,16 @@ import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
-import { FiltroArrayPipe } from './filtro-array.pipe'
+import { FiltroArrayPipe } from './filtro-array.pipe';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
     CamelCasePipe,
-    FiltroArrayPipe
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe
   ],
   imports: [
     BrowserModule,
